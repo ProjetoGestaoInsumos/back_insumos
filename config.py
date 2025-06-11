ef6dc0ef0c5636f4cefa57bfb4f41f8520fe1eaf
Eduardo Simas <eduardosimas12@gmail.com>
2025-06-11 17:00:19 -0300

    feat: adicionar funcionalidade de envio de e-mail com PDF

--- config.py
+++ config.py
@@ -5,5 +5,8 @@ load_dotenv()
 class Settings:
     DATABASE_URL: str = os.getenv("DATABASE_URL")
     SECRET_KEY: str = os.getenv("SECRET_KEY", "defaultsecret")
+    
+    EMAIL_ADDRESS: str = os.getenv("EMAIL_ADDRESS")
+    EMAIL_PASSWORD: str = os.getenv("EMAIL_PASSWORD")
 
 settings = Settings()
